7da2791ec44e1b61c53f6cdd714fbba4619157fe
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-17 14:59:18 +0200

    feat: added secondary constructor & overrode toString

--- app/src/main/java/com/example/campuswrapper/LectureContributor.kt
+++ app/src/main/java/com/example/campuswrapper/LectureContributor.kt
@@ -1,4 +1,10 @@
 package com.example.campuswrapper
 
-class LectureContributor (val title: String?, val firstName : String, val lastName: String, val email: String?, val phone: String?, val office: String, val profile: String?){
+class LectureContributor (val title: String?, val firstName : String, val lastName: String, val email: String?, val phone: String?, val office: String?, val profile: String?){
+    constructor(firstName: String, lastName: String) : this(null, firstName, lastName, null, null, null, null)
+
+    override fun toString(): String {
+        return "LectureContributor(title=$title, firstName='$firstName', lastName='$lastName', email=$email, phone=$phone, office=$office, profile=$profile)"
+    }
+
 }
