14bf1476731f1985bbf871849c5bcd2b390a8bca
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-25 16:51:53 +0200

    feat: modified Curricular property-list
    Updated the type of the lectureID attribute and added a suggestion attribute of type String.

--- app/src/main/java/com/example/campuswrapper/structure/lectures/Curricular.kt
+++ app/src/main/java/com/example/campuswrapper/structure/lectures/Curricular.kt
@@ -1,4 +1,4 @@
 package com.example.campuswrapper.structure.lectures
 
-class Curricular (val study: String, val topic: String, val subTopic: String, val lecture: Lecture) {
+class Curricular (val study: String, val topic: String, val subTopic: String, val lectureID: String, val suggestion: String?) {
 }
