1ec6dc59aa78333145e50ccdcc550564c4d0d123
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-28 12:33:13 +0200

    feat: added toString override - Exam

--- app/src/main/java/com/example/campuswrapper/structure/exam/Exam.kt
+++ app/src/main/java/com/example/campuswrapper/structure/exam/Exam.kt
@@ -10,4 +10,8 @@ class Exam(
     val notes: ExamNotes,
     val mode: ExamMode,
     val href: String
-)
+){
+    override fun toString(): String {
+        return "Exam(lecture_ID='$lecture_ID', lecture_Name='$lecture_Name', start=$start, end=$end, location='$location', notes=$notes, mode=$mode, href='$href')"
+    }
+}
