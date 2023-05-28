b0b48fc5fe66b97cfad3d853ac0d04d1b1d8d3c1
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-28 11:50:10 +0200

    feat: implemented basic-exam-list activity to fetch and display exams

--- app/src/main/java/com/example/campuswrapper/BasicExamList.kt
+++ app/src/main/java/com/example/campuswrapper/BasicExamList.kt
@@ -1,11 +1,48 @@
 package com.example.campuswrapper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.campuswrapper.adapters.ExamListAdapter
+import com.example.campuswrapper.structure.exam.Exam
+import com.example.campuswrapper.structure.fetch.Handler
+import com.example.campuswrapper.structure.fetch.SearchCriteria
+import com.example.campuswrapper.structure.fetch.SemesterType
+import com.example.campuswrapper.structure.lectures.Lecture
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 
 class BasicExamList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_exam_list)
+
+        val header = findViewById<TextView>(R.id.txtHeading)
+        header.text = "List of exams"
+
+        Thread {
+            val basicExams = Handler.fetchExams(SearchCriteria(2022, SemesterType.SUMMER, 687))
+            if(basicExams != null){
+                Log.d("Campus-Layout", "Exams: ${basicExams.get(0).lecture_Name}")
+                runOnUiThread {
+                    showExams(basicExams)
+                }
+            }else{
+                runOnUiThread {
+                    Snackbar.make(findViewById(R.id.txtHeading), "Failed to fetch exams!", Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }.start()
     }
+
+    private fun showExams(exams: ArrayList<Exam>) {
+        val recycleList = findViewById<RecyclerView>(R.id.recycleExamLIstBasic)
+        recycleList.layoutManager = LinearLayoutManager(this)
+        recycleList.adapter = ExamListAdapter(this, exams)
+    }
+
 }
